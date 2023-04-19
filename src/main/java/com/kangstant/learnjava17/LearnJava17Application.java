1c482e7896142fd618a4254e13dc4447ad8ab814
Spencer Huang <spencer.huang@nio.com>
2023-04-19 11:08:08 +0800

    git init

---
+++ src/main/java/com/kangstant/learnjava17/LearnJava17Application.java
@@ -0,0 +1,13 @@
+package com.kangstant.learnjava17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJava17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnJava17Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kangstant/learnjava17/LearnJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kangstant.learnjava17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJava17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
